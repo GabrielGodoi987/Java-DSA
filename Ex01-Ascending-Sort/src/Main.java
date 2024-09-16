a080eaa0d8d216315b8e3d5e93bfa7720d081664
GanbrielGodoi390 <110495432+GabrielGodoi987@users.noreply.github.com>
2024-09-16 12:25:12 -0500

    init of sorting stack

--- Ex01-Ascending-Sort/src/Main.java
+++ Ex01-Ascending-Sort/src/Main.java
@@ -1,6 +1,16 @@
+import javax.swing.*;
 import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
+        int stackSize = Integer.parseInt(JOptionPane.showInputDialog("Quantos items estarão na pilha ?"));
+        int numbers;
+        MyStack<Integer> stack = new MyStack<Integer>(stackSize);
+
+        for(int i = 0; i < stackSize; i++) {
+            numbers = Integer.parseInt(JOptionPane.showInputDialog("Digite o elemento"));
+            stack.addItem(numbers);
+            System.out.println(stack.unstack());
+        }
     }
 }
